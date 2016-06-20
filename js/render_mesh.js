7ea1dfea54a39c2eabc49cdacee7b7f71f01719c
Dillon Huff <dillonhuff@gmail.com>
2016-06-20 11:36:43 -0700

    Can now render a mesh from a json string

--- js/render_mesh.js
+++ js/render_mesh.js
@@ -5,12 +5,38 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+var parse_pts = function(jpts) {
+    var ptArray = [];
+    for (var i = 0; i < jpts.length; i++) {
+	var v = new THREE.Vector3(jpts[i][0], jpts[i][1], jpts[i][2]);
+	ptArray.push(v);
+    }
+    return ptArray;
+};
+
+var parse_faces = function(jfaces) {
+    var ptArray = [];
+    for (var i = 0; i < jfaces.length; i++) {
+	ptArray.push(new THREE.Face3(jfaces[i][0], jfaces[i][1], jfaces[i][2]));
+    }
+    return ptArray;
+};
+
+var build_three_mesh = function(json_mesh) {
+    //alert(json_mesh);
+    var jm = JSON.parse(json_mesh);
+//    alert(jm.pts);
+    var pts = parse_pts(jm.pts);
+//    alert(pts);
+    var faces = parse_faces(jm.faces);
+//    alert(faces);
+    return {"pts" : pts, "faces" : faces};
+}
+
 var build_mesh = function(json_mesh) {
-    var m = {"pts" : [new THREE.Vector3(-.20, .20, 0),
-		      new THREE.Vector3(-.20, -.20, 0),
-		      new THREE.Vector3(.20, -.20, 0)],
-	     "faces" : [new THREE.Face3(0, 1, 2)]};
-    
+//    alert(json_mesh);
+    var m = build_three_mesh(json_mesh);
+    alert(m);
     var geometry = new THREE.Geometry();
     geometry.vertices = m.pts;
     geometry.faces = m.faces;
